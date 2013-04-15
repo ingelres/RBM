080a2559c2db31c8ef4030450062b29950027ead
ingelres <github@fingelrest.net>
2013-04-15 10:07:20 +0200

    Code cleanup

--- js/libtags.js
+++ js/libtags.js
@@ -1,13 +1,12 @@
 function jsfunc_tidIsDescendant(tid, ptid)
 {
-    while(true)
+    while((tid = rbm_tid_parents[tid]) != undefined)
     {
-        parentTid = rbm_tid_parents[tid];
-
-             if(parentTid == ptid)      return true;
-        else if(parentTid == undefined) return false;
-        else                            tid = parentTid;
+        if(tid == ptid)
+            return true;
     }
+
+    return false;
 }
 
 function jsfunc_getHierarchy(tid)
