212688e1f5067bf39d06c92a788d690119ef694d
ingelres <github@fingelrest.net>
2013-04-24 17:38:48 +0200

    Updated strings

--- inc/l10n.php
+++ inc/l10n.php
@@ -5,5 +5,5 @@ var L10N = {
     name_empty:                 "<?=_('Please enter a name')?>",
     name_exists:                "<?=_('This name is already used')?>",
     confirm_delete_tag:         "<?=_('Delete this tag?')?>",
-    confirm_delete_tag_subtags: "<?=_('This tag has subtags associated to it. They will be deleted as well.<br />Delete this tag?')?>",
+    confirm_delete_tag_subtags: "<?=_('This tag has subtags associated to it. They will be deleted as well.<br /><br />Delete this tag?')?>",
 };
