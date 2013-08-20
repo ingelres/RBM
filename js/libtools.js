7a24837126f58aa655a6f28d555891ae8d09198d
ingelres <github@fingelrest.net>
2013-08-20 14:43:21 +0200

    Added comments

--- js/libtools.js
+++ js/libtools.js
@@ -2,6 +2,13 @@ var libtools = (function(){
 
     var my = {};
 
+    /**
+     * Escape special HTML characters (similar to PHP htmlspecialchars()).
+     *
+     * @param str The string to be escaped.
+     *
+     * @return The escaped string.
+    **/
     my.jsfunc_htmlspecialchars = function(str)
     {
         return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
